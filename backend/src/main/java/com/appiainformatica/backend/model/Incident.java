1be6314f9e971d78e6bc1642178be794212c65e6
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-13 20:36:09 -0300

    feat(model): implementar auditoria automática e suporte a arrays.
    - Implementa JpaAuditing para campos de auditoria como data da criação e sempre que o campo for modificado, será persistido a data e a hora no banco, com as anotações (@createdDate e @LastModifiedDate) e o @EntityListeners(AuditingEntityListener.class) para ouvirquando uma entidade for persistida e modificada.
    - Utiliza biblioteca externa (Hypersistence Utils) para fazer  conversão de tipo. No caso do banco de dados (TEXT[]) e na aplicação uma lista de String.
    - Modificaca os nomes dos atributos de auditoria, para melhor entendimento e padronização.
    - Implementa anotações do jakarta.persistence como (@Column e @Enumerated).

--- backend/src/main/java/com/appiainformatica/backend/model/Incident.java
+++ backend/src/main/java/com/appiainformatica/backend/model/Incident.java
@@ -2,9 +2,14 @@ package com.appiainformatica.backend.model;
 
 import com.appiainformatica.backend.model.enums.Priority;
 import com.appiainformatica.backend.model.enums.Status;
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -12,33 +17,47 @@ import java.util.UUID;
 @Entity
 @Table(name = "incident")
 @Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Incident {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "description")
     private String description;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "priority")
     private Priority priority;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
     private Status status;
 
+    @Column(name = "responsible_email")
     private String responsibleEmail;
 
+    @Type(ListArrayType.class)
+    @Column(name = "tags", columnDefinition = "text[]")
     private List<String> tags;
 
-    private LocalDateTime openingDate;
+    @CreatedDate
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
 
-    private LocalDateTime updateDate;
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
 
     public Incident() {}
 
     public Incident(UUID id, String title, String description, Priority priority,
                     Status status, String responsibleEmail, List<String> tags,
-                    LocalDateTime openingDate, LocalDateTime updateDate) {
+                    LocalDateTime createdDate, LocalDateTime updatedDate) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -46,7 +65,7 @@ public class Incident {
         this.status = status;
         this.responsibleEmail = responsibleEmail;
         this.tags = tags;
-        this.openingDate = openingDate;
-        this.updateDate = updateDate;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
     }
 }
