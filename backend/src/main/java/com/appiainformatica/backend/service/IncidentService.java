8c2813e8146871d5d55bb92ad674ef68e1c530ac
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-15 11:00:41 -0300

    feat: Implementar lógica no service para busca com filtros e salvar incident
    - Adiciona método que retorna uma lista onde contém a lógica para fazer as buscas utilizando os filtros criados na classe IncidentSpecs.
    - importa os métodos estáticos da classe IncidentSpecs para não precisar chamá-la toda vez que for utilizar um método.
    - cria método para salvar um incident e retorna um objeto persistido.

--- backend/src/main/java/com/appiainformatica/backend/service/IncidentService.java
+++ backend/src/main/java/com/appiainformatica/backend/service/IncidentService.java
@@ -1,7 +1,14 @@
 package com.appiainformatica.backend.service;
 
+import com.appiainformatica.backend.model.Incident;
+import com.appiainformatica.backend.model.enums.Priority;
+import com.appiainformatica.backend.model.enums.Status;
 import com.appiainformatica.backend.repository.IncidentRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
+import static com.appiainformatica.backend.repository.specs.IncidentSpecs.*;
 
 @Service
 public class IncidentService {
@@ -10,6 +17,27 @@ public class IncidentService {
 
     public IncidentService(IncidentRepository repository) {
         this.repository = repository;
+    }
+
+    public Incident saveIncident(Incident incident) {
+        return repository.save(incident);
+    }
+
+    public List<Incident> getIncidentsPerFilter(Status status, Priority priority, String q){
+
+        Specification<Incident> specs = Specification
+                .allOf((root, query, cb) -> cb.conjunction());
+
+        if (status != null) {
+            specs = specs.and(statusEqual(status));
+        }
+        if (priority != null) {
+            specs = specs.and(priorityEqual(priority));
+        }
+        if (q != null){
+            specs = specs.and(titleOrDescriptionContains(q));
+        }
+        return repository.findAll(specs);
     }
 
 }
