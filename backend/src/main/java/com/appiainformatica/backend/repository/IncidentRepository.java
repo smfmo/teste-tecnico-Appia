d61ca14ee24ed71972dd46f27960ce4588687b42
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-15 10:41:45 -0300

    feat: habilitar Specifications no IncidentRepository
    - Extende JpaSpecificationExecutor<T> pra permitir consultas dinâmicas.

--- backend/src/main/java/com/appiainformatica/backend/repository/IncidentRepository.java
+++ backend/src/main/java/com/appiainformatica/backend/repository/IncidentRepository.java
@@ -2,9 +2,10 @@ package com.appiainformatica.backend.repository;
 
 import com.appiainformatica.backend.model.Incident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface IncidentRepository extends JpaRepository<Incident, UUID>{
+public interface IncidentRepository extends JpaRepository<Incident, UUID>, JpaSpecificationExecutor<Incident>{
 }
