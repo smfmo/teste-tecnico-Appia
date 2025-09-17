28c2a824082a1f6924050b3afada6c4a65a017d7
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-17 00:13:10 -0300

    feat: adicionar componente incidents
    - implementa componente incidents

---
+++ frontend/src/app/incidents/incidents.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@Component({
+  selector: 'app-incidents',
+  imports: [
+    CommonModule, RouterModule, ReactiveFormsModule,
+    MatTableModule, MatPaginatorModule, MatSortModule,
+    MatFormFieldModule, MatInputModule, MatSelectModule,
+    MatButtonModule, MatIconModule, MatChipsModule, MatCardModule, MatBadgeModule
+  ],
+  templateUrl: './incidents.component.html',
+  styleUrl: './incidents.component.scss'
+})
+export class IncidentsComponent implements OnInit {
+
+  public ngOnInit(): void {
+      
+  }
+
+}
