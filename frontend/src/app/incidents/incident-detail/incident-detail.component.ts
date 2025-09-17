4038053f5aea94726155e51b610233dbc4d5f363
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-17 00:26:49 -0300

    feat: adicionar estrutura de componentes Incident
    - Cria pasta incidents que vai conter todos os componente necessários para o projeto.
    - Cria componente incident-detail para exibir detalhes e comentários de um incident.
    - Cria componente incident-list para listar incidents com busca, paginação e filtros.

---
+++ frontend/src/app/incidents/incident-detail/incident-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-detail',
+  imports: [],
+  templateUrl: './incident-detail.component.html',
+  styleUrl: './incident-detail.component.scss'
+})
+export class IncidentDetailComponent {
+
+}
---
+++ frontend/src/app/incidents/incident-list/incident-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-list',
+  imports: [],
+  templateUrl: './incident-list.component.html',
+  styleUrl: './incident-list.component.scss'
+})
+export class IncidentListComponent {
+
+}
