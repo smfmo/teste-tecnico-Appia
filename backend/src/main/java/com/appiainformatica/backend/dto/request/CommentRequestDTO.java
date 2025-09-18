7d25f4e65995856f9a12fc41e6ee0fe42e7a6c82
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-18 12:53:10 -0300

    feat: Adicionar anotações de validações dos campos dos Dtos
    - Adiciona validações nos campos de CommentRequestDto
    - Adiciona validações nos campos de IncidentRequestDto

--- backend/src/main/java/com/appiainformatica/backend/dto/request/CommentRequestDTO.java
+++ backend/src/main/java/com/appiainformatica/backend/dto/request/CommentRequestDTO.java
@@ -1,6 +1,13 @@
 package com.appiainformatica.backend.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CommentRequestDTO(
+        @NotBlank(message = "Campo obrigatório")
+        @Size(min = 5, max = 120, message = "Campo fora do tamanho padrão")
         String author,
+        @NotBlank(message = "Campo obrigatório")
+        @Size(min = 5, message = "Campo fora do tamanho padrão")
         String message
 ) {}
--- backend/src/main/java/com/appiainformatica/backend/dto/request/IncidentRequestDTO.java
+++ backend/src/main/java/com/appiainformatica/backend/dto/request/IncidentRequestDTO.java
@@ -2,13 +2,23 @@ package com.appiainformatica.backend.dto.request;
 
 import com.appiainformatica.backend.model.enums.Priority;
 import com.appiainformatica.backend.model.enums.Status;
+import jakarta.validation.constraints.*;
+
 import java.util.List;
 
 public record IncidentRequestDTO(
+        @NotBlank(message = "Campo obrigatório")
+        @Size(min = 5, max = 120, message = "Campo fora do tamanho padrão")
         String title,
+        @NotBlank(message = "Campo obrigatório")
+        @Size(min = 5, message = "Campo fora do tamanho padrão")
         String description,
+        @NotNull(message = "Prioridade é obrigatório")
         Priority priority,
+        @NotNull(message = "Status é obrigatório")
         Status status,
+        @Email(message = "Email inválido, insira um email válido")
         String responsibleEmail,
+        @NotEmpty(message = "tags obrigatórias")
         List<String> tags
 ) {}
