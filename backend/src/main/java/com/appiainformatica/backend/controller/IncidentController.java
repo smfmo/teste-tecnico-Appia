16ff0e90090024ced64c25231daeb890d9bac3e4
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-18 10:50:10 -0300

    refactor: simplificar incidentController e mapeamento de DTOs
    - Renomeia getIncidents para getIncidentsPerFilter
    - injeta commentMapper e ajusta nomes das dependências
    - injeta CommentService e renomeia IncidentService para maior clareza

--- backend/src/main/java/com/appiainformatica/backend/controller/IncidentController.java
+++ backend/src/main/java/com/appiainformatica/backend/controller/IncidentController.java
@@ -1,15 +1,22 @@
 package com.appiainformatica.backend.controller;
 
+import com.appiainformatica.backend.dto.request.CommentRequestDTO;
 import com.appiainformatica.backend.dto.request.IncidentRequestDTO;
+import com.appiainformatica.backend.dto.response.CommentResponseDTO;
 import com.appiainformatica.backend.dto.response.IncidentResponseDTO;
+import com.appiainformatica.backend.mapper.CommentMapper;
 import com.appiainformatica.backend.mapper.IncidentMapper;
+import com.appiainformatica.backend.model.Comment;
 import com.appiainformatica.backend.model.Incident;
 import com.appiainformatica.backend.model.enums.Priority;
 import com.appiainformatica.backend.model.enums.Status;
+import com.appiainformatica.backend.service.CommentService;
 import com.appiainformatica.backend.service.IncidentService;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -17,23 +24,21 @@ import java.util.UUID;
 @RequestMapping("/incidents")
 public class IncidentController {
 
-    private final IncidentService service;
-    private final IncidentMapper mapper;
+    private final IncidentService incidentService;
+    private final IncidentMapper incidentMapper;
+    private final CommentService commentService;
+    private final CommentMapper commentMapper;
 
-    public IncidentController(IncidentService service, IncidentMapper mapper) {
-        this.service = service;
-        this.mapper = mapper;
-    }
-
-    @PostMapping
-    public ResponseEntity<Incident> saveIncident(@RequestBody IncidentRequestDTO incidentRequestDTO) {
-        Incident entitySaved = mapper.toEntity(incidentRequestDTO);
-        service.saveIncident(entitySaved);
-        return ResponseEntity.ok().build();
+    public IncidentController(IncidentService incidentService, IncidentMapper incidentMapper,
+                              CommentService commentService, CommentMapper commentMapper) {
+        this.incidentService = incidentService;
+        this.incidentMapper = incidentMapper;
+        this.commentService = commentService;
+        this.commentMapper = commentMapper;
     }
 
     @GetMapping
-    public ResponseEntity<Page<IncidentResponseDTO>> getIncidents(
+    public ResponseEntity<Page<IncidentResponseDTO>> getIncidentsPerFilter(
             @RequestParam(value = "status", required = false) Status status,
             @RequestParam(value = "priority", required = false) Priority priority,
             @RequestParam(value = "q", required = false) String q,
@@ -41,15 +46,16 @@ public class IncidentController {
             @RequestParam(value = "page-size", defaultValue = "10") Integer pageSize,
             @RequestParam(value = "sort", required = false) String sort) {
 
-        Page<Incident> pageResponse = service.getIncidentsPerFilter(status, priority, q, page, pageSize, sort);
-        Page<IncidentResponseDTO> result = pageResponse.map(mapper::toDto);
+        Page<Incident> pageResponse = incidentService.getIncidentsPerFilter(status, priority, q, page, pageSize, sort);
+        Page<IncidentResponseDTO> result = pageResponse.map(incidentMapper::toDto);
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<IncidentResponseDTO>> getIncidentById(@PathVariable("id") UUID id) {
-        Optional<Incident> response = service.getIncidentById(id);
-        Optional<IncidentResponseDTO> result = response.map(mapper::toDto);
+        Optional<Incident> response = incidentService.getIncidentById(id);
+        Optional<IncidentResponseDTO> result = response.map(incidentMapper::toDto);
         return ResponseEntity.ok(result);
     }
+
 }
