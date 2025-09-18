b479f4e402a3f81d7c42f66a5d19ab9e29b96e27
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-18 07:14:20 -0300

    feat: adicionar método auxiliar que faz o sorting na busca e filtragem
    - Adiciona o método auxiliar que recebe os parâmetros:
      - page
      - pageSize
      - sort
    e encapsula a lógica de sorting e paginação.
    - Atualiza método getIncidentsPerFilter para receber o novo parâmetro sort e utilizar o método auxiliar

--- backend/src/main/java/com/appiainformatica/backend/service/IncidentService.java
+++ backend/src/main/java/com/appiainformatica/backend/service/IncidentService.java
@@ -4,15 +4,15 @@ import com.appiainformatica.backend.model.Incident;
 import com.appiainformatica.backend.model.enums.Priority;
 import com.appiainformatica.backend.model.enums.Status;
 import com.appiainformatica.backend.repository.IncidentRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 import java.util.UUID;
-
 import static com.appiainformatica.backend.repository.specs.IncidentSpecs.*;
 
 @Service
@@ -26,7 +26,7 @@ public class IncidentService {
 
     public Page<Incident> getIncidentsPerFilter(
             Status status, Priority priority, String q,
-            Integer page, Integer pageSize){
+            Integer page, Integer pageSize, String sort){
 
         Specification<Incident> specs = Specification
                 .allOf((root, query, cb) -> cb.conjunction());
@@ -40,8 +40,27 @@ public class IncidentService {
             specs = specs.and(titleOrDescriptionContains(q));
         }
 
-        Pageable pageRequest = PageRequest.of(page,pageSize);
+        Pageable pageRequest = createPageRequest(page, pageSize, sort);
         return repository.findAll(specs, pageRequest);
+    }
+
+    private Pageable createPageRequest(Integer page, Integer pageSize, String sort) {
+        if (sort == null || sort.isEmpty()) {
+            return PageRequest.of(page, pageSize);
+        }
+        String[] sortParams = sort.split(",");
+
+        if (sortParams.length != 2) {
+            return PageRequest.of(page, pageSize);
+        }
+
+        String field = sortParams[0].trim();
+        String direction = sortParams[1].trim().toUpperCase();
+
+        Sort.Direction sortDirection = direction.equals("DESC")
+                ? Sort.Direction.DESC
+                : Sort.Direction.ASC;
+        return  PageRequest.of(page, pageSize, Sort.by(sortDirection, field));
     }
 
     public void saveIncident(Incident incident) {
