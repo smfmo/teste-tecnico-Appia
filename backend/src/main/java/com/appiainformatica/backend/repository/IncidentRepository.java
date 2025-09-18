93a5ae2fe9a3d319e232891f2d52cee168333e73
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-09-18 10:27:03 -0300

    feat: Adicionar métodos de contagem por status e prioridade em IncidentRepository
    - Adiciona countByStatus(Status status)para contar incidents por status.
    - Adiciona countByPriority(Priority priority) para contar incidents por prioridade.

--- backend/src/main/java/com/appiainformatica/backend/repository/IncidentRepository.java
+++ backend/src/main/java/com/appiainformatica/backend/repository/IncidentRepository.java
@@ -1,6 +1,8 @@
 package com.appiainformatica.backend.repository;
 
 import com.appiainformatica.backend.model.Incident;
+import com.appiainformatica.backend.model.enums.Priority;
+import com.appiainformatica.backend.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface IncidentRepository extends JpaRepository<Incident, UUID>, JpaSpecificationExecutor<Incident>{
+    long countByStatus(Status status);
+    long countByPriority(Priority priority);
 }
